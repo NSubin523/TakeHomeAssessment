10dc697d54767a004a264cf0707362a82c0989a4
Subin Neupane <subinneupane@Subins-MacBook-Pro.local>
2021-09-29 14:06:54 -0400

    Filter feature added

---
+++ app/src/main/java/com/example/assesment/data/DataClass2.kt
@@ -0,0 +1,4 @@
+package com.example.assesment.data
+
+class DataClass2 {
+}
---
+++ app/src/main/java/com/example/assesment/screens/displayImages/ApplyFilters.kt
@@ -0,0 +1,11 @@
+package com.example.assesment.screens.displayImages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplyFilters : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apply_filters)
+    }
+}
